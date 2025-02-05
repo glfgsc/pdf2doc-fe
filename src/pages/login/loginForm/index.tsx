cc4837d2d8730ac322330e806db04a6c38f8c5d9
unknown <guan.lefu@opg.cn>
2025-02-05 18:32:06 +0800

    Update

--- src/pages/login/loginForm/index.tsx
+++ src/pages/login/loginForm/index.tsx
@@ -1,9 +1,29 @@
 import styles from './index.less';
+import { Tabs } from 'antd';
+import type { TabsProps } from 'antd';
 
 const LoginForm = () => {
+
+    const items: TabsProps['items'] = [
+        {
+          key: 'password',
+          label: '账号密码登录',
+          children: <div>
+1
+          </div>,
+        },
+        {
+          key: 'email',
+          label: '邮箱登录',
+          children: <div>
+            2
+          </div>
+        }
+    ];
+
     return (
         <div className={styles.loginFormConatiner}>
-123
+            <Tabs defaultActiveKey="password" items={items}  />
         </div>
     );
 }
